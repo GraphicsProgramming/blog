81a32d0eb66ef58c459e9909eb58828e8583b0e9
deccer <decccer@outlook.com>
2024-10-26 17:10:52 +0200

    Change baseUrl to /blog/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
   url: 'https://graphicsprogramming.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // This might cause trouble during local development, switch to '/' then
+  baseUrl: '/blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
