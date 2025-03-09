c33f8d4ee4d30574d6dab0736a1f1e065d62ca77
Teo Fumagalli <teo.fum@gmail.com>
2025-03-09 15:02:56 -0300

    froglist

--- gen-webring-routes.js
+++ gen-webring-routes.js
@@ -1,7 +1,17 @@
 const fs = require("fs");
 
-function makeRoute() {
-    fs.appendFileSync('./static/test.html', 'Hello world!');
+// Import ze froges
+const frogs = require("static/webring/froglist.json");
+
+function makeRoutes(frog, nextFrog, prevFrog) {
+    fs.appendFileSync(`./static/webring/frogs/${frog.name}.html`, frog.url);
+    fs.appendFileSync(`./static/webring/frogs/${frog.name}/next.html`, nextFrog.url);
+    fs.appendFileSync(`./static/webring/frogs/${frog.name}/prev.html`, prevFrog.url);
 }
 
-makeRoute();
+frogs.forEach((frog, i) => {
+    const nextFrog = frogs.at((i + 1) % frogs.length);
+    const prevFrog = frogs.at(i - 1); // array.at(-1) returns the last element
+
+    makeRoutes(frog, nextFrog, prevFrog);
+});
