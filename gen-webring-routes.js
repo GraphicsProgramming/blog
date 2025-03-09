b4ea9e80cb3e116a869a6ff952fcd7434123e07b
Teo Fumagalli <teo.fum@gmail.com>
2025-03-09 15:21:49 -0300

    make html redirects

--- gen-webring-routes.js
+++ gen-webring-routes.js
@@ -3,11 +3,31 @@ const fs = require("fs");
 // Import ze froges
 const frogs = require("./static/webring/froglist.json");
 
+function makeHtmlRedirect(frog) {
+    return `
+    <!DOCTYPE HTML>
+    <html lang="en-US">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="refresh" content="0; url=${frog.url}">
+            <script type="text/javascript">
+                // JS redirect as fallback if the meta tag doesn't work
+                window.location.href = "${frog.url}"
+            </script>
+            <title>Page Redirection</title>
+        </head>
+        <body>
+            <!-- And a link, just in case -->
+            If you are not redirected automatically, follow this <a href='${frog.url}'>link</a>.
+        </body>
+    </html>`;
+}
+
 function makeRoutes(frog, nextFrog, prevFrog) {
     fs.mkdirSync(`./static/webring/frogs/${frog.name}`, { recursive: true });
-    fs.appendFileSync(`./static/webring/frogs/${frog.name}.html`, frog.url);
-    fs.appendFileSync(`./static/webring/frogs/${frog.name}/next.html`, nextFrog.url);
-    fs.appendFileSync(`./static/webring/frogs/${frog.name}/prev.html`, prevFrog.url);
+    fs.appendFileSync(`./static/webring/frogs/${frog.name}.html`, makeHtmlRedirect(frog));
+    fs.appendFileSync(`./static/webring/frogs/${frog.name}/next.html`, makeHtmlRedirect(nextFrog));
+    fs.appendFileSync(`./static/webring/frogs/${frog.name}/prev.html`, makeHtmlRedirect(prevFrog));
 }
 
 frogs.forEach((frog, i) => {
